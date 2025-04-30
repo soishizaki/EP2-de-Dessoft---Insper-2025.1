6ab9cc3e330f491ae57b690d6834012ebf1fbaa5
soishizaki <sofiasi1@al.insper.edu.br>
2025-04-30 15:58:34 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,8 @@ def rolar_dados(quantidade):
         resultado = random.randint(1, 6)  
         resultados.append(resultado)
     return resultados
+
+def guardar_dado(dados_rolados, dados_guardados, indice_para_guardar):
+    dado_escolhido = dados_rolados.pop(indice_para_guardar)
+    dados_guardados.append(dado_escolhido)
+    return [dados_rolados, dados_guardados]
