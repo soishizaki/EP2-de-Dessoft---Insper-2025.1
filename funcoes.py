adbac1664fe27f69e86b328e952f813da552aca0
soishizaki <sofiasi1@al.insper.edu.br>
2025-05-01 13:31:56 -0300

    exercicio 4

--- funcoes.py
+++ funcoes.py
@@ -16,3 +16,12 @@ def remover_dado(dados_rolados, dados_guardados, indice_para_remover):
     dado = dados_guardados.pop(indice_para_remover)
     dados_rolados.append(dado)
     return [dados_rolados, dados_guardados]
+
+def calcula_pontos_regra_simples(dados):
+    pontuacoes = {}
+    for face in range(1, 6): 
+        soma_face = sum(dado for dado in dados if dado == face)
+        pontuacoes[face] = soma_face
+
+    return pontuacoes
+
