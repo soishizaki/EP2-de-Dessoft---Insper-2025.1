a9451e9984e03fc856c580bf4ef6dd2de73ef932
soishizaki <sofiasi1@al.insper.edu.br>
2025-05-06 10:07:38 -0300

    cartela de pontuação

--- programa.py
+++ programa.py
@@ -1,3 +1,17 @@
+#Nesse exercício vamos tentar ir passo a passo para tentar evitar erros. 
+
 from funcoes import rolar_dados, guardar_dado, remover_dado, calcula_pontos_regra_simples, calcula_pontos_soma, calcula_pontos_sequencia_baixa, calcula_pontos_sequencia_alta, calcula_pontos_full_house, calcula_pontos_quadra, calcula_pontos_quina, calcula_pontos_regra_avancada, faz_jogada, imprime_cartela
 
-
+cartela_de_pontuação = {
+    'regra_simples': {
+        1: -1, 2: -1, 3: -1, 4: -1, 5: -1, 6: -1
+    },
+    'regra_avancada': {
+        'sem_combinacao': -1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
